699c41f7ebc2ac5de0fbfd42a8a5a070dba93512
Jinseok Kim <kemjensak@gmail.com>
2022-05-06 23:17:17 +0900

    updated

--- src/trajectory_recorder.py
+++ src/trajectory_recorder.py
@@ -6,6 +6,8 @@ import sys
 import carla
 import rospy
 import numpy as np
+from datetime import datetime
+import csv
 
 from math import sin, radians, sqrt
 distance_between_lane = 3.5/2
@@ -24,20 +26,23 @@ def main():
     actors = world.get_actors().filter('vehicle.toyota.*')
 
     location = actors[0].get_location()
+    location.z = 0.0
     initial_time = last_time = rospy.Time.now()
-   
+    now = datetime.now()
     starting_waypoint = map.get_waypoint(location, project_to_road=True)
-    starting_waypoint_coordinate = np.array([starting_waypoint.transform.location.x, starting_waypoint.transform.location.x, 0])
+    starting_waypoint_coordinate = np.array([starting_waypoint.transform.location.x, starting_waypoint.transform.location.y, 0])
     starting_lane_id = starting_waypoint.lane_id
 
     left_lane_id = starting_waypoint.get_left_lane().lane_id
     right_lane_id = starting_waypoint.get_right_lane().lane_id
+    filename = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+    f = open('/home/irol/ros_ws/catkin_ws/src/carla_sim/op_rollout_modifier/export/write_'+ filename +'.csv','a')
+    wr = csv.writer(f)
     
     while not rospy.is_shutdown():
 
         current_time = rospy.Time.now()
-        d_time = current_time - last_time
-        last_time = current_time
+        elapsed_time = (current_time - initial_time).to_sec()
         current_location = actors[0].get_location()
         current_location.z = 0.0
         nearest_waypoint = map.get_waypoint(current_location, project_to_road=True)
@@ -57,46 +62,26 @@ def main():
         
         
         if nearest_waypoint.lane_id == starting_lane_id:
-            distance = nearest_waypoint.transform.location.distance(current_location)
-            if is_left_or_right < 0: distance *= -1      
+            y_distance = nearest_waypoint.transform.location.distance(current_location)
+            if is_left_or_right > 0: y_distance *= -1      
         elif nearest_waypoint.lane_id >= right_lane_id:
-            distance = nearest_waypoint.get_left_lane().transform.location.distance(current_location)
+            y_distance = -nearest_waypoint.get_left_lane().transform.location.distance(current_location)
         elif nearest_waypoint.lane_id <= right_lane_id:
-            distance = -nearest_waypoint.get_right_lane().transform.location.distance(current_location)
+            y_distance = nearest_waypoint.get_right_lane().transform.location.distance(current_location)
 
-        np.dot(next_wp_vector, nearest_wp_heading_vector)/np.linalg.norm(next_wp_vector)
-        np.linalg.norm(next_wp_vector)
+        x_distance = np.linalg.norm(nearest_waypoint_coordinate - starting_waypoint_coordinate)
 
-        
-        print(nearest_waypoint.next(0.01)[0].transform.location)
-        print(np.linalg.norm(nearest_waypoint_coordinate))
-        print(current_location)
-        print(distance)
-        print(left_lane_id, nearest_waypoint.lane_id, right_lane_id)
-        print(d_time.to_sec())
-        print((current_time - initial_time).to_sec())
+        print(x_distance)
+        print(y_distance)
+        # print(left_lane_id, nearest_waypoint.lane_id, right_lane_id)
+        # print((current_time - initial_time).to_sec())
         # distance between lanes are 3.5m 
-
+        
+        wr.writerow([elapsed_time, x_distance, y_distance])
         
         world.wait_for_tick()
-            
-
-        # host_waypoint = waypoint
-        # route_distance = 2
-        # previous_waypoint = host_waypoint.previous(route_distance)[0]
-        # next_waypoint = host_waypoint.next(route_distance)[0]
-        # _transform = next_waypoint.transform
-        # _location, _rotation  = _transform.location, _transform.rotation
-        # x1, y1 = _location.x, _location.y
-        # yaw1 = _rotation.yaw
-
-        # _transform = previous_waypoint.transform
-        # _location, _rotation  = _transform.location, _transform.rotation
-        # x2, y2 = _location.x, _location.y
-        # yaw2 = _rotation.yaw
-
-        # c = 2*sin(radians((yaw1-yaw2)/2)) / sqrt((x1-x2)**2 + (y1-y2)**2)
-        # print(c)
+        
+    f.close()
 
 
 
@@ -104,3 +89,4 @@ def main():
 if __name__ == '__main__':
 
     main()
+    
